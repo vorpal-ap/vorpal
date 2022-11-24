0de0e3b6554325d3b1a2e456b3b016cc35a0a3a0
auntPSYCHO <81919558+auntPSYCHO@users.noreply.github.com>
2022-11-24 12:02:47 +0530

    I SWEAR TO GOD IM ABOUT TO COMMIT MASS GENOCIDE

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-function ani() {
+function buttonCLick() {
     document.getElementById('sendButton').classList.add('animated');
     setTimeout(function () {
         document.getElementById('sendButton').classList.remove('animated')
     },300);
-    dis();
+    showPopup();
 }
 
-function dis() {
+function showPopup() {
     document.getElementById('textArea').setAttribute('disabled','');
     document.getElementById('textArea').style.opacity = '0.3';
     document.getElementById('textArea').style.cursor = 'not-allowed';
     document.getElementById('popup').style.opacity = '1';
     document.getElementById('popup').style.animationName = 'popUp';
     setTimeout(function () {
+        document.getElementById('textArea').focus();
         document.getElementById('textArea').removeAttribute('disabled');
         document.getElementById('textArea').style.opacity = '1';
         document.getElementById('textArea').style.removeProperty('cursor');
-        document.getElementById('popup').style.animationName = 'popOut';
         document.getElementById('popup').style.opacity = '0';
-        document.getElementById('textArea').focus();
+        document.getElementById('popup').style.animationName = 'popOut';
         $('#textArea').val('');
     },3000);
 }
