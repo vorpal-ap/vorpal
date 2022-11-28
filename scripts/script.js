db53aa2666a613b5784f091749747bc9f80b9c70
auntPSYCHO <81919558+auntPSYCHO@users.noreply.github.com>
2022-11-28 23:18:39 +0530

    Added dark-light theme button.

--- scripts/script.js
+++ scripts/script.js
@@ -19,6 +19,36 @@ function showPopup() {
         document.getElementById('textArea').style.removeProperty('cursor');
         document.getElementById('popup').style.opacity = '0';
         document.getElementById('popup').style.animationName = 'popOut';
-        $('#textArea').val('');
+        $('textArea').val('');
     },3000);
+}
+
+function switchOn() {
+    if (document.getElementById('themeSwitch').style.marginLeft === '50px') {
+        document.getElementById('themeSwitch').style.marginLeft = '0'
+        darkMode(false)
+    } else {
+        document.getElementById('themeSwitch').style.marginLeft = '50px'
+        darkMode(true)
+    }
+}
+
+function darkMode(boolean) {
+    if (boolean) {
+        document.documentElement.style.setProperty('--black', '#dddddd');
+        document.documentElement.style.setProperty('--grey', '#bcbdbd');
+        document.documentElement.style.setProperty('--hay', '#8ca0cd');
+        document.documentElement.style.setProperty('--cyan', '#dd5c60');
+        document.documentElement.style.setProperty('--white', '#0c101f');
+        document.getElementById("themeSwitch").innerHTML = "Light";
+    }
+    else {
+        document.documentElement.style.setProperty('--black', '#222222');
+        document.documentElement.style.setProperty('--grey', '#434242');
+        document.documentElement.style.setProperty('--hay', '#735F32');
+        document.documentElement.style.setProperty('--cyan', '#22A39F');
+        document.documentElement.style.setProperty('--white', '#F3EFE0');
+        document.getElementById("themeSwitch").innerHTML = "Dark";
+    }
+
 }
